4a18a42402a9689e8e2342b57c2795eb6b5b0cdb
Berry-san <108697745+Berry-san@users.noreply.github.com>
2022-10-15 03:30:17 +0100

    added style

--- index.php
+++ index.php
@@ -6,6 +6,83 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="/style.css" />
     <title>Login</title>
+    <style>
+        * {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Raleway", sans-serif;
+  background-color: rgb(165, 15, 226);
+}
+form {
+  margin: 10rem 40rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid rgb(165, 15, 226);
+  border-radius: 10px;
+  padding: 2rem;
+  background-color: white;
+  width: 50rem;
+}
+.form-control h1 {
+  margin: 2rem;
+}
+.inputs {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+  width: 40rem;
+}
+
+.inputs label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+.inputs input {
+  height: 2rem;
+  border-radius: 7px;
+  padding: 1rem;
+}
+.inputs select {
+  height: 2rem;
+  border-radius: 7px;
+  padding: 1rem;
+}
+.color {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+  width: 40rem;
+}
+
+.color label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+.color input {
+  height: 2rem;
+  border-radius: 7px;
+  padding: 0.2rem;
+}
+button {
+  padding: 1rem 2.5rem;
+  border-radius: 7px;
+  text-align: right;
+  font-size: 1.5rem;
+  color: white;
+  font-weight: bold;
+  background-color: rgb(165, 15, 226);
+  border: 1px solid rgb(165, 15, 226);
+}
+::placeholder {
+  font-size: 1rem;
+}
+
+    </style>
   </head>
   <body>
     <form class="form-control" action="user_data.php" method="POST">
